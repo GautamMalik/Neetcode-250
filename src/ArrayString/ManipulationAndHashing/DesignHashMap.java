7661746b64c601f886008eb049405297cb38b0a8
malik <malikgautam2@gmail.com>
2026-07-16 18:06:46 +0530

    committing new ques

--- src/ArrayString/ManipulationAndHashing/DesignHashMap.java
+++ src/ArrayString/ManipulationAndHashing/DesignHashMap.java
@@ -2,5 +2,86 @@ package ArrayString.ManipulationAndHashing;
 
 // ques: https://neetcode.io/problems/design-hashmap/question
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DesignHashMap {
+
+    List<List<Pair>> list;
+    int cap=15000;
+
+    public DesignHashMap() {
+        list = new ArrayList<>(cap);
+        for(int i=0; i<=cap; i++){
+            list.add(new ArrayList<>());
+        }
+    }
+
+    public void put(int key, int value) {
+        int idx = key%cap;
+        for(Pair pair : list.get(idx)){
+            if(pair.key == key) {
+                pair.value = value;
+                return;
+            }
+        }
+        list.get(idx).add(new Pair(key,value));
+    }
+
+    public int get(int key) {
+        int idx = key%cap;
+        for(Pair pair : list.get(idx)){
+            if(pair.key == key)
+                return pair.value;
+        }
+        return -1;
+    }
+
+    public void remove(int key) {
+        int idx = key%cap;
+        List<Pair> bucket = list.get(idx);
+        for (int i = 0; i < bucket.size(); i++) {
+            if (bucket.get(i).key == key) {
+                bucket.remove(i);
+                return;
+            }
+        }
+    }
+
+    private static class Pair{
+        int key;
+        int value;
+
+        Pair(int key, int value){
+            this.key= key;
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    private static class DesignHashMapBrute {
+
+        int[] arr;
+
+        public DesignHashMapBrute() {
+            arr= new int[1000000];
+            Arrays.fill(arr,-1);
+        }
+
+        public void put(int key, int value) {
+            arr[key] = value;
+        }
+
+        public int get(int key) {
+            return arr[key];
+        }
+
+        public void remove(int key) {
+            arr[key] = -1;
+        }
+    }
 }
--- src/ArrayString/ManipulationAndHashing/DesignHashset.java
+++ src/ArrayString/ManipulationAndHashing/DesignHashset.java
@@ -2,5 +2,58 @@ package ArrayString.ManipulationAndHashing;
 
 // ques: https://neetcode.io/problems/design-hashset/question?list=neetcode250
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DesignHashset {
+
+    private List<List<Integer>> list;
+    private final int cap = 15000;
+
+    public DesignHashset() {
+        list = new ArrayList<>(cap);
+        for (int i = 0; i < cap; i++) {
+            list.add(new ArrayList<>());
+        }
+    }
+
+    public void add(int key) {
+        int idx = key % cap;
+        if(!list.get(idx).contains(key))
+            list.get(idx).add(key);
+    }
+
+    public void remove(int key) {
+        int idx = key % cap;
+        list.get(idx).remove(Integer.valueOf(key));
+    }
+
+    public boolean contains(int key) {
+        int idx = key % cap;
+        for(int i : list.get(idx)){
+            if(i == key)
+                return true;
+        }
+        return false;
+    }
+}
+
+class DesignHashsetBruteForce {
+    boolean[] arr;
+
+    public DesignHashsetBruteForce() {
+        arr = new boolean[1000000];
+    }
+
+    public void add(int key) {
+        arr[key] = true;
+    }
+
+    public void remove(int key) {
+        arr[key] = false;
+    }
+
+    public boolean contains(int key) {
+        return arr[key];
+    }
 }
